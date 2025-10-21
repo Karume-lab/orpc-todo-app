0d8a6ef5c893ec3dcb691264d8d4e7ab4c344572
Karume-lab <danielkarume@outlook.com>
2025-10-21 20:09:14 +0300

    feat: :adhesive_bandage: pluck all the strawberries (remove all the errors)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>page</div>;
+import { orpcTanstackClient } from "@/utils/orpc";
+import { useQuery } from "@tanstack/react-query";
+
+const TodosPage = () => {
+  const {
+    data: todos,
+    isLoading,
+    error,
+  } = useQuery(orpcTanstackClient.todo.getAll.queryOptions());
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h1>TodosPage</h1>
+      <ul>
+        {todos?.map((t) => (
+          <li key={t.id}>{t.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-export default page;
+export default TodosPage;
